ad1552868cbd8026ac48dcd995b5602ac833a325
Punyawat18 <81132911+Punyawat18@users.noreply.github.com>
2025-02-08 23:54:54 +0700

    forgot to add file :(

--- Game/CardGame.py
+++ Game/CardGame.py
@@ -1,15 +1,13 @@
-import pygame
 import random
 import os
-import sys
 import json
-import atexit
+import pygame
 
 try:
     logging_path = os.path.join(os.path.dirname(__file__), "GameHistory")
 except NameError:
     os.mkdir(f'{os.path.dirname(__file__)}/GameHistory')
-    logging_path = os.path.join(os.path.dirname(__file__))
+    logging_path = os.path.join(os.path.dirname(__file__), "GameHistory")
 
 round_count = None
 try:
@@ -25,13 +23,14 @@ except FileNotFoundError:
 
 #create log file
 with open(f'{logging_path}/{round_count}.json', 'w') as f:
-    pass
+    json.dump({}, f)  # Initialize with an empty dictionary
 
 attack_card_name = ["Charge", "Punch", "Strike", "Slash", "Stab", "Fireball", "Lightning Bolt", "Ice Shard", "Wind Blade", "Earthquake"]
 defense_card_name = ["Block", "Dodge", "Parry", "Deflect", "Absorb", "Reflect", "Counter", "Barrier", "Guardian", "Shield"]
 heal_card_name = ["Heal", "Regenerate", "Recover", "Revive", "Renew", "Cure", "Potion", "Elixir", "Salve", "Bandage"]
 turn_count = 0
-
+move_count = 0
+Whose_move = None
 
 class Card:
     def __init__(self, name, cost, type, point_value, card_id):
@@ -70,7 +69,6 @@ class Player:
             self.hand.append(card)
             self.deck.remove(card)
             
-
     def discard(self, card_id):
         discard_success = False
         for card in self.hand:
@@ -90,14 +88,12 @@ class Player:
                 self.deck.append(restore_card)
             self.used = []
 
-
     def play(self, card_id):
         for card in self.hand:
             if card.card_id == card_id:
                 self.hand.remove(card)
                 self.used.append(card)
                 
-
     def show_hand(self):
         for card in self.hand:
             print(card)
@@ -113,7 +109,6 @@ class Player:
                 self.hand.remove(card)
                 self.used.append(card)
 
-#need change
     def take_damage(self, damage_taken):
         if self.defense > damage_taken:
             self.defense -= damage_taken
@@ -148,36 +143,88 @@ def Create_deck():
         card_num += 1
     return deck
 
-"""
-def graphics():
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Card Game")
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((250, 250, 250))
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-"""
-## to be changed in move detect way
+def restore_game_state(player, enemy, game_number, turn, move):
+    with open(f'{logging_path}/{game_number}.json', 'r') as log_file:
+        log_data = json.load(log_file)
+        ## enumerate turn
+        for turn_data in log_data["Game_Data"]:
+            ## check if turn is the same
+            try:
+                if turn_data["Turn_Count"] == turn:
+                    print(turn_data)
+                    for move_data in turn_data["Turn_Data"]:
+                        print(move_data["Move_Count"])
+                        try:
+                            if move_data["Move_Count"] == move:
+                                player.health = move_data["Move_Data"]["player"]["health"]
+                                player.defense = move_data["Move_Data"]["player"]["defense"]
+                                player.energy = move_data["Move_Data"]["player"]["energy"]
+                                player.hand = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["player"]["hand"]]
+                                player.used = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["player"]["used"]]
+                                player.discarded = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["player"]["discarded"]]
+
+                                enemy.health = move_data["Move_Data"]["enemy"]["health"]
+                                enemy.defense = move_data["Move_Data"]["enemy"]["defense"]
+                                enemy.energy = move_data["Move_Data"]["enemy"]["energy"]
+                                enemy.hand = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["enemy"]["hand"]]
+                                enemy.used = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["enemy"]["used"]]
+                                enemy.discarded = [Card(card["name"], card["cost"], card["type"], card["point_value"], card["card_id"]) for card in move_data["Move_Data"]["enemy"]["discarded"]]
+                        except:
+                            pass
+            except:
+                pass
+
+
+
+def show_rewind_options():
+    game_files = []
+    for file in os.listdir(logging_path):
+        if file.endswith(".json"):
+            game_files.append(file)
+    game_files.sort(key=lambda x: int(x.split('.')[0]))
+    if not game_files:
+        print("No previous games available for rewind.")
+        return None
+
+    print("Available games to rewind:")
+    for i, game_file in enumerate(game_files):
+        print(f'{i + 1}: {game_file}')
+
+    game_choice = int(input("Choose a game to rewind (number): "))
+    if game_choice < 1 or game_choice > len(game_files):
+        print("Invalid game choice.")
+        return None
+
+    for game_file in game_files:
+        if f'{game_choice}.json' == game_file:
+            with open(f'{logging_path}/{game_file}', 'r') as f:
+                log_data = json.load(f)
+                print(f'Game {game_choice} has {log_data["Game_Data"][-1]["Turn_Count"]} turns.')
+                turn_choice = int(input(f'Choose a turn to rewind to from 1 to {log_data["Game_Data"][-1]["Turn_Count"]}: '))
+                if turn_choice < 1 or turn_choice > log_data["Game_Data"][-1]["Turn_Count"]:
+                    print("Invalid turn choice.")
+                    return None
+                move_choice = int(input(f'Choose a move to rewind to from 1 to {log_data["Game_Data"][turn_choice - 1]["Turn_Data"]["Move_Count"]}: '))
+                if move_choice < 1 or move_choice > log_data["Game_Data"][turn_choice - 1]["Turn_Data"]["Move_Count"]:
+                    print("Invalid move choice.")
+                    return None
+                return [game_choice, turn_choice, move_choice]
+
 def Turn(player, enemy):
+    global turn_count, move_count, Whose_move
+    Whose_move = "player"
+    turn_count += 1
+    move_count = 0
     print(f"{player.name}'s turn")
     player.energy = 5
     player.draw()
 
     action = None
     while True:
-
-        logging(player, enemy)
-        action = int(input("Choose an action: 1(Play), 2(Discard), 3(End Turn), 4(Check Status of Both Player)\n"))
+        action = int(input("Choose an action: 1(Play), 2(Discard), 3(End Turn), 4(Check Status of Both Player), 5(Rewind)\n"))
         if action == 1:
-            action_status = False
+            move_count += 1
             player.show_hand()
             card_id = int(input("Enter the card number to use it : "))
             for card in player.hand:
@@ -192,25 +239,34 @@ def Turn(player, enemy):
                             player.healing(card.card_id)
                         print("Card Played")
                         player.play(card.card_id)
-                        action_status = True
+                        logging(player, enemy, turn_count, move_count)
                         break
                     else:
                         print("Not enough energy")
-                        action_status = True
                         break
-            if action_status:
-                continue
-            print("Card not exist or not in hand")
+            else:
+                print("Card not exist or not in hand")
         elif action == 2:
+            move_count += 1
             print("----------Current Hand----------")
             player.show_hand()
             print("------------Card Used-----------")
             player.show_used()
             card_id = int(input("Please Choose a Card to discard from above: "))
             player.discard(card_id)
+            logging(player, enemy, turn_count, move_count)
         elif action == 4:
             print(f'You({player})')
             print(f'Enemy({enemy})')
+        elif action == 5:
+            print("Temporarily disabled")
+            continue
+            rewind_choice = None
+            #debugging
+            while rewind_choice == None:
+                rewind_choice = show_rewind_options()
+            game_number, turn, move = rewind_choice
+            restore_game_state(player, enemy, game_number, turn, move)
         elif action == 3:
             break
         else:
@@ -220,12 +276,17 @@ def Turn(player, enemy):
     print(f'Enemy({enemy})')
 
 def enemy_turn(player, enemy):
+    global turn_count, move_count, Whose_move
+    Whose_move = "enemy"
+    turn_count += 1
+    move_count = 0
     print(f"{enemy.name}'s turn")
     enemy.energy = 5
     enemy.draw()
     end_using = False
-    logging(player, enemy)
+    logging(player, enemy, turn_count, move_count)
     while enemy.energy > 0:
+        move_count += 1
         not_played = 0
         for card in enemy.hand:
             if card.cost <= enemy.energy:
@@ -237,6 +298,7 @@ def enemy_turn(player, enemy):
                     enemy.healing(card.card_id)
                 print(f"{enemy.name} played {card.name}")
                 enemy.play(card.card_id)
+                logging(player, enemy, turn_count, move_count)
                 break
             else:
                 not_played += 1
@@ -253,16 +315,10 @@ def enemy_turn(player, enemy):
         print("End of turn\n")
         break
 
-def logging(player, enemy):
-    player_hand = {}
-    for card in player.hand:
-        player_hand.update(card.show())
-    player_used = {}
-    for card in player.used:
-        player_used.update(card.show())
-    player_discarded = {}
-    for card in player.discarded:
-        player_discarded.update(card.show())
+def logging(player, enemy, turn_count, move_count):
+    player_hand = [card.show() for card in player.hand]
+    player_used = [card.show() for card in player.used]
+    player_discarded = [card.show() for card in player.discarded]
 
     player_dict = {
         "name": player.name,
@@ -273,9 +329,50 @@ def logging(player, enemy):
         "used": player_used,
         "discarded": player_discarded
     }
-    with open(f'{logging_path}/{round_count}.json', 'a') as f:
-        f.write(json.dumps(player_dict, indent=4))
 
+    enemy_hand = [card.show() for card in enemy.hand]
+    enemy_used = [card.show() for card in enemy.used]
+    enemy_discarded = [card.show() for card in enemy.discarded]
+
+    enemy_dict = {
+        "name": enemy.name,
+        "health": enemy.health,
+        "defense": enemy.defense,
+        "energy": enemy.energy,
+        "hand": enemy_hand,
+        "used": enemy_used,
+        "discarded": enemy_discarded
+    }
+
+
+    with open(f'{logging_path}/{round_count}.json', 'r+') as f:
+        log_data = json.load(f)
+        if "Game_Data" not in log_data:
+            log_data["Game_Data"] = [{
+                "Turn_Count": turn_count,
+                "Turn_Data": {
+                    "Move_Count": move_count,
+                    "Whose_Move": Whose_move,
+                    "Move_Data": {
+                        "player": player_dict,
+                        "enemy": enemy_dict
+                    }
+                }
+            }]
+        else:
+            log_data["Game_Data"].append({
+                "Turn_Count": turn_count,
+                "Turn_Data": {
+                    "Move_Count": move_count,
+                    "Whose_Move": Whose_move,
+                    "Move_Data": {
+                        "player": player_dict,
+                        "enemy": enemy_dict
+                    }
+                }
+            })
+        f.seek(0)
+        json.dump(log_data, f, indent=4)
 
 def main():
     Create_deck()
@@ -293,5 +390,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-   # with open(f'{logging_path}/log.txt', 'w') as f:
