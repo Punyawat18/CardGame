87751482e6e3b5843c05d54a12614c3c7e11bf82
Punyawat18 <81132911+Punyawat18@users.noreply.github.com>
2025-02-08 23:53:28 +0700

    can save the game, cant rewind it yet

--- Game/test.py
+++ Game/test.py
@@ -1,3 +1,7 @@
-import random
+import json
+import os
 
-print(random.sample(range(1, 10), 5))
+path = os.path.join(os.path.dirname(__file__), "GameHistory")
+with open(f'{path}/12.json', 'r') as f:
+    data = json.load(f)
+    print(data['Game_Data'][2])
